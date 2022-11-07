64a6964cb83857ad8dc7fc084a11fc2556b0e898
jgaffuri <jgaf@protonmail.com>
2022-11-07 15:47:49 +0100

    a

--- src/main/java/eu/europa/ec/eurostat/euronym/EuroNymeProduction.java
+++ src/main/java/eu/europa/ec/eurostat/euronym/EuroNymeProduction.java
@@ -38,7 +38,7 @@ public class EuroNymeProduction {
 
 	//TODO check / remove duplicates
 	//TODO correct paris position
-	//TODO check Epinal and Luxeuil-les-Bains
+	//TODO check Luxeuil-les-Bains
 
 	//TODO use stat atlas - for multi-ling ? https://ec.europa.eu/statistical-atlas/arcgis/rest/services/Basemaps/StatAtlas_Cities_Labels_2014/MapServer/0/query?where=POPL_SIZE%3E50000&outSR=3035&inSR=3035&geometry=3428439.0697888224,2356253.0645389506,4339693.4974049805,2548197.243346825&geometryType=esriGeometryEnvelope&f=json&outFields=STTL_NAME,POPL_SIZE
 	//TODO add other aggregates: EFTA, UE, etc.
