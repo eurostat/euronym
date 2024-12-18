b9514fbf8c058c8b1b4f972e290ccd53a4412831
jgaffuri <jgaf@proton.me>
2024-12-18 10:04:38 +0100

    a

--- src/main/java/eu/europa/ec/eurostat/euronym/EuroNymeProduction.java
+++ src/main/java/eu/europa/ec/eurostat/euronym/EuroNymeProduction.java
@@ -380,7 +380,7 @@ public class EuroNymeProduction {
 		System.out.println(nt.size() + " features loaded");
 		CoordinateReferenceSystem crsNT = GeoData.getCRS(nt_);
 
-		List<String> cntsRegio = Arrays.asList(new String[]{ "RO", "BA", "AL", "ME", "HU", "RS" });
+		List<String> cntsRegio = Arrays.asList(new String[]{ "RO", "BA", "AL", "ME", "HU", "RS", "BU", "Xk" });
 		for (Feature f : nt) {
 			Feature f_ = new Feature();
 
